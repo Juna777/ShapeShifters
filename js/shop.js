738bb3b25940c15e1cc709a268136bbf361e1138
Juna777 <142022967+Juna777@users.noreply.github.com>
2023-10-17 16:49:58 +0800

    Update shop.js
    Update shop.js const url..get the address from github itself.

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-const url = `http://127.0.0.1:5500/js/api.js`;
+const url = `https://github.com/Juna777/ShapeShifters/blob/main/js/api.js`;
 
 fetch(url)
 .then( response => response.json()) //object
