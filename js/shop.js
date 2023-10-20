ad9bbf0981ec17f9c05a7bd6776d3da20c701385
Juna777 <142022967+Juna777@users.noreply.github.com>
2023-10-20 17:35:34 +0800

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-const url = `https://juna777.github.io/ShapeShifters/data/js/api.js`;
+const url = `https://juna777.github.io/ShapeShifters/data/api.js`;
 
 fetch(url)
 .then( response => response.json())
